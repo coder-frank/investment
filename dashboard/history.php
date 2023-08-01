904d5978f99e8c48bce796bd40a5c6f94a0bc314
coder-frank <frankemmanuel249@gmail.com>
2023-08-01 10:53:47 +0100

    table

--- dashboard/history.php
+++ dashboard/history.php
@@ -3,8 +3,30 @@ require_once '../universal/includes/header.php';
 require_once '../universal/includes/sidebar.php';
 ?>
 
-
+<h1>History</h1>
+<br>
+<div class="table-responsive">
+	<table class="table">
+		<thead class="table-dark">
+			<tr>
+				<th scope="col">S/N</th>
+				<th scope="col">Type</th>
+				<th scope="col">Amount</th>
+				<th scope="col">Date</th>
+				<th scope="col">Status</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<th scope="row">1</th>
+				<td>Recharge</td>
+				<td>₦5000</td>
+				<td>01/08/2023</td>
+				<td class="alert-success">Success</td>
+			</tr>
+		</tbody>
+	</table>
+</div>
 
 
 <?php require_once '../universal/includes/footer.html'; ?>
-https://prod.liveshare.vsengsaas.visualstudio.com/join?EFAA8118380A100A934FF2D33BB34C928C53
